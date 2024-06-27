aca215d0d1f367a72460ea6a5bcbf8caf3c35219
macareonie <ryanchiang189@gmail.com>
2024-06-27 23:42:00 +0800

    Create NavBar, Footer and Routes
    Using TailwindCSS, a navigation bar containing routes to other pages
    have been added.
    A Footer fixed to the bottom of the screen has been added.

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="fixed bottom-0 w-full bg-black text-white px-4 p-10">
+        Footer
+      </div>
+    </>
+  );
+}
---
+++ src/pages/foodblog/FoodBlog.jsx
@@ -0,0 +1,8 @@
+export default function FoodBlog() {
+  return (
+    <>
+      <div>Food Blog</div>
+      <p>food recipes and random stuff</p>
+    </>
+  );
+}
