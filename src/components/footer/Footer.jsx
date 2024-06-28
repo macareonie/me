ada1e6cd703405d01cd625267ee8131fe85224a9
macareonie <ryanchiang189@gmail.com>
2024-06-28 13:20:24 +0800

    Add footer with relevant social links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
+import { FaDiscord, FaTelegram, FaInstagram } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <>
-      <div className="fixed bottom-0 w-full bg-black text-white px-4 p-10">
-        Footer
-      </div>
+      <footer className="fixed bottom-0 left-0 right-0 bg-gray-800 p-4">
+        <div className="flex justify-center space-x-4">
+          <a
+            href="https://discordapp.com/users/299909491120537600/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaDiscord className="text-white h-6 w-6 hover:text-blue-500" />
+          </a>
+          <a
+            href="https://t.me/macaReonie"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTelegram className="text-white h-6 w-6 hover:text-blue-400" />
+          </a>
+          <a
+            href="https://www.instagram.com/reon.chiang/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="text-white h-6 w-6 hover:text-pink-500" />
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
