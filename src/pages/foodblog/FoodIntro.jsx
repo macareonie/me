e42bb0031b39ce35813bda42886b948831837a7c
macareonie <ryanchiang189@gmail.com>
2024-09-04 21:05:36 +0800

    Update content
    Added information regarding current details: Current courses etc.
    Added new recipes
    Added pictures for tips and recipes

--- src/pages/foodblog/FoodIntro.jsx
+++ src/pages/foodblog/FoodIntro.jsx
@@ -2,15 +2,8 @@ export default function FoodIntro() {
   return (
     <div className="bg-gray-100 dark:bg-gray-900">
       {/* Top Section */}
-      <div className="flex flex-col md:flex-row items-center min-h-screen p-8 bg-white dark:bg-gray-800">
-        <div className="w-full md:w-1/2 flex justify-center mb-8 md:mb-0 ">
-          <img
-            src="https://via.placeholder.com/300"
-            alt="Profile"
-            className="w-64 h-64 rounded-full shadow-lg"
-          />
-        </div>
-        <div className="w-full md:w-1/2 text-center md:text-left">
+      <div className="relative min-h-screen flex items-center justify-center p-8 bg-white dark:bg-gray-800">
+        <div className="w-full md:w-1/2 text-center md:text-center">
           <h1 className="text-4xl font-bold mb-4 dark:text-gray-100">
             If you can read, you can cook
           </h1>
@@ -24,15 +17,19 @@ export default function FoodIntro() {
           <br />
           <p className="text-lg text-gray-700 dark:text-gray-300">
             This food blog will showcase several tips and tricks I've picked up
-            from online videos and articles ever since the Covid-19 pandemic
-            which prompted my self-teaching homecooking journey. Included will
-            also be some of my go-to recipes for meals I personally cook at
-            home. Enjoy!
+            from Youtube and various online recipes ever since the Covid-19
+            pandemic which prompted my self-teaching homecooking journey.
+            Included will be some of my go-to recipes for meals I personally
+            cook at home, as well as some more adventurous recipes I've
+            attempted. Enjoy!
           </p>
           <br />
           <p className="text-lg text-gray-700 dark:text-gray-300">
             Disclaimer: Most of the recipes do not have exact steps or
-            measurements as I mainly cook of feel and vibes.
+            measurements as I mainly cook of feel and vibes. The pictures used
+            below are also mostly from online sources. The ones that I took
+            myself shoudl be pretty obvious 🥲. Food photography is clearly
+            something I have not learnt yet. Hopefully, one day...
           </p>
         </div>
       </div>
